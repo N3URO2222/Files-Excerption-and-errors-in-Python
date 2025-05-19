801e9c7a33d0929e4fb0e2cc877f6e0bddc9d9fd
N3URO2222 <as9563937@gmail.com>
2025-05-19 17:49:43 +0530

    Update file_open_close.py

--- file_open_close.py
+++ file_open_close.py
@@ -1,4 +1,5 @@
-'''
+#TASK 1: READ A FILE AND HANDLE ERRORS
+
 file1= open('my_file.txt','r')
 #use to open a file and need to close close it
 reading_file = file1.read()
@@ -9,11 +10,11 @@ file1.close()
 #another way to open and edit the file
 #with open('my_file.txt','w') as file1:
     #statements
-'''
+
 from fileinput import close
 
 #add data to a file, remember it clears all the existing data in the file
-'''
+
 file1= open('my_file.txt','w')
 writing_file= file1.write('Hey')
 print(writing_file)
@@ -23,9 +24,19 @@ file1= open('my_file.txt','r')
 reading_file = file1.read()
 print(reading_file)
 file1.close()
-'''
+
+#HANDLE AN ERROR
+try:
+    with open("non_existent_file.txt", "r") as file:
+        content = file.read()
+except FileNotFoundError:
+    print("Oops! The file does not exist. Please check the file name or path.")
+
+
+#TASK 2: WRITE AND APPEND DATA TO A FILE
+
 #appending it helps us by writing in a a file without clearing its contents
-'''
+
 file1=open('my_file.txt','a')
 file_writing=file1.write('\nAhoy There welcome')
 #print(file_writing)#it prints the no. of statements in the file
@@ -35,7 +46,7 @@ file1= open('my_file.txt','r')
 reading_file = file1.read()
 print(reading_file)
 file1.close()
-'''
+
 
 #read and write
 file=open('my_file.txt','r+')
